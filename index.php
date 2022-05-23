716e456f6ccf3495482922179c66aa621c39030e
kml92 <kevinloveee@gmail.com>
2022-05-23 10:28:42 -0700

    init commit

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php // silence is golden ?>
+<?php
+/**
+ * Plugin Name: WordPress Plugin
+ * Version: 1.0.0
+ * Plugin URI: ///
+ * Description: This is your starter template for your next WordPress plugin.
+ * Author: FYCLABS
+ *
+ * Text Domain: wordpress-plugin
+ *
+ * @package WordPress
+ * @author FYCLabs
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once 'plugin-updater.php';
+if ( is_admin() ) {
+  new BFIGitHubPluginUpdater( __FILE__, 'myGitHubUsername', "Repo-Name" );
+}
