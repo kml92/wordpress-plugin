d6341aa2b4cb96a1a41fcb001547cf00510e1343
kml92 <kevinloveee@gmail.com>
2022-05-23 11:19:57 -0700

    init commit

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * Plugin Name: WordPress Plugin
- * Version: 1.0.0
  * Plugin URI: ///
  * Description: This is your starter template for your next WordPress plugin.
  * Author: FYCLABS
@@ -18,6 +17,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once 'plugin-updater.php';
-if ( is_admin() ) {
-  new BFIGitHubPluginUpdater( __FILE__, 'kml92', 'wordpress-plugin', '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' );
-}
+// include_once plugin_dir_path(__FILE__) . '/path/to/PDUpdater.php';
+$updater = new PDUpdater(__FILE__);
+$updater->set_username('kml92');
+$updater->set_repository('wordpress-plugin');
+$updater->authorize('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');
+$updater->initialize();
